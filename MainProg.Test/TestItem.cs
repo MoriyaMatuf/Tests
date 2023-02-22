27e8c83c785a19d75fd3b05c32c7df136e9bb54e
MoriyaMatuf <85495810+MoriyaMatuf@users.noreply.github.com>
2023-02-22 13:07:59 +0200

    change

--- MainProg.Test/TestItem.cs
+++ MainProg.Test/TestItem.cs
@@ -3,26 +3,41 @@ using System.Collections.Generic;
 using System.Text;
 using NUnit.Framework;
 namespace MainProg.Test
-{[TestFixture]
+{
+    [TestFixture]
     public class TestItem
     {
         [Test]
-        [Category ("UNitTest-check Action") ]
+        //name test->
+        [Category("UNitTest-check Action")]
+        //לשחק מי לפני מי בסדר הטסטים
+        [Order(1)]
+        //פונקצייה שתרוץ בתחילת הקוד בדרך כלל משמשת לאיתחול db
+        [SetUp]
         public void TestPlus()
         {
             Items items = new Items();
 
-            int ret=items.Plus(5,4);
-            Assert.AreEqual(9, ret);
+            int ret = items.Plus(5, 4);
+            //value בודק את ה
+            Assert.AreEqual(9, ret, "the result is good");
+            //בודק את הרפרנס אותו דבר
+            Assert.AreSame(ret, ret);
         }
-
-        [Test]
+        //  לא יבצע תטסט הזה לבינתיים 
+        [Test, Ignore("i am not finish")]
         public void TestMinus()
         {
             Items items = new Items();
 
             int ret = items.Minus(5, 4);
             Assert.AreEqual(1, ret);
+            //אם בטווח
+            //Assert.That(ret, Is.InRange(1 - 0));
+            //גדול מ
+            //Assert.That(ret, Is.AtLeast(-1));
+            //קטן /=
+            //Assert.That(ret, Is.AtMost(100));
         }
 
 
